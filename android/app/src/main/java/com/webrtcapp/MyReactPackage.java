0a2b78701dcdc2c6e30fedb52160c985631c2ca9
ctq123 <1654282805@qq.com>
2024-10-23 12:49:31 +0800

    fix: 自定义模块

--- android/app/src/main/java/com/webrtcapp/MyReactPackage.java
+++ android/app/src/main/java/com/webrtcapp/MyReactPackage.java
@@ -11,6 +11,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 自定义模块
+ */
 public class MyReactPackage implements ReactPackage{
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
